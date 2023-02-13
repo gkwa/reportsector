00f048ba8552ae1a7d936677e86c95795b8ea983
Taylor Monacelli <taylormonacelli@gmail.com>
2023-02-13 08:00:29 -0800

    make package

--- src/reportsector/cli.py
+++ src/reportsector/cli.py
@@ -26,6 +26,8 @@ import sys
 
 from reportsector import __version__
 
+from . import main as mainmod
+
 __author__ = "Taylor Monacelli"
 __copyright__ = "Taylor Monacelli"
 __license__ = "MIT"
@@ -38,23 +40,6 @@ _logger = logging.getLogger(__name__)
 # Python scripts/interactive interpreter, e.g. via
 # `from reportsector.skeleton import fib`,
 # when using this Python module as a library.
-
-
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
-
 
 # ---- CLI ----
 # The functions defined in this section are wrappers around the main Python
@@ -72,13 +57,18 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
+    parser = argparse.ArgumentParser(
+        description=(
+            "Make it easier to iterate on installing"
+            " kubernetes by generating bash scripts"
+        )
+    )
     parser.add_argument(
         "--version",
         action="version",
         version="reportsector {ver}".format(ver=__version__),
     )
-    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
+
     parser.add_argument(
         "-v",
         "--verbose",
@@ -123,7 +113,7 @@ def main(args):
     args = parse_args(args)
     setup_logging(args.loglevel)
     _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
+    mainmod.main()
     _logger.info("Script ends here")
 
 
